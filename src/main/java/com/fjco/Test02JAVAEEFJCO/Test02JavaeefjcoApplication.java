6a377ce42b91406f9255b3135ff977639b76a7fb
Fernando-Jose-Claros-Orellana <ferinocente357@gmail.com>
2024-08-07 10:21:20 -0600

    Se inicializa proyecto

---
+++ src/main/java/com/fjco/Test02JAVAEEFJCO/Test02JavaeefjcoApplication.java
@@ -0,0 +1,13 @@
+package com.fjco.Test02JAVAEEFJCO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test02JavaeefjcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test02JavaeefjcoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fjco/Test02JAVAEEFJCO/Test02JavaeefjcoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fjco.Test02JAVAEEFJCO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test02JavaeefjcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
